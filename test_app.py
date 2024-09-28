80a6e556541220fb901ba350220ca64b2dcdab93
jesus farias <36554510+jafr0691@users.noreply.github.com>
2024-09-27 21:35:13 -0400

    Update test_app.py
    cambios para revisar

--- test_app.py
+++ test_app.py
@@ -3,15 +3,15 @@ from noticias_cristianas import app
 
 @pytest.fixture
 def client():
-    # Configura Flask en modo de prueba
     app.config['TESTING'] = True
-    
-    # Usa el cliente de prueba que provee Flask
     with app.test_client() as client:
         yield client
 
 def test_home(client):
-    """Prueba que la ruta '/' responda con '¡Hola, Mundo!'."""
+    """Prueba que la ruta '/testimonio-del-dia' responda con '¡Hola, Mundo!'."""
     response = client.get('/testimonio-del-dia')
+    print(f"Response Status Code: {response.status_code}")
+    print(f"Response Data: {response.data}")
+    
     assert response.status_code == 200
-    assert b"Hola, Mundo!".decode('utf-8') in response.data.decode('utf-8')
+    assert b'¡Hola, Mundo!' in response.data
