33061beff60b93ab79f056e34302cb1b084f7993
jesus farias <36554510+jafr0691@users.noreply.github.com>
2024-09-29 00:07:38 -0400

    Update noticias_cristianas.py
    eliminar puertos demas

--- noticias_cristianas.py
+++ noticias_cristianas.py
@@ -299,6 +299,4 @@ def get_info():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=4000, debug=True)
